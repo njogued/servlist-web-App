94b306de62f0e6762414cfbf48e59a470781a09e
njogued <njogued@gmail.com>
2023-09-11 00:16:29 +0300

    Edited views to render the templates

--- my_project/users/views.py
+++ my_project/users/views.py
@@ -13,20 +13,21 @@ def user_signup(request):
         email = request.POST['email']
         password = request.POST['password']
         user_name = f"{first_name}{randint(1, 999)}"
-        if User.objects.filter(user_name=user_name).exists():
-            return HttpResponse("User taken")
+        if User.objects.filter(email=email).exists():
+            return HttpResponse("User email already exists. Try another email")
         else:
-            new_user = User.objects.create_user(
+            new_user = User(
                 first_name=first_name,
                 last_name=last_name,
                 email=email,
                 password=password,
                 user_name=user_name
-                )
+            )
             new_user.save()
             return HttpResponse("User Created")
-    return HttpResponse("template needed")
-        
+    return render(request, "signup.html")
+
+
 def user_login(request):
     if request.method == "POST":
         user_name = request.POST['user_name']
@@ -37,7 +38,8 @@ def user_login(request):
             return HttpResponse("Logged in")
         else:
             return redirect('/login')
-            
+
+
 def user_logout(request):
     logout(request)
     return redirect('/')
