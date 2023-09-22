8f42f3bd0e3be7cc0a111d214327b7eceafbd0db
njogued <njogued@gmail.com>
2023-09-22 11:58:09 +0300

    Removed unused templates path in settings file
    Updated requirements.txt

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -126,7 +126,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'static/users'),
-    os.path.join(BASE_DIR, 'static/businesses'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -135,5 +134,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
