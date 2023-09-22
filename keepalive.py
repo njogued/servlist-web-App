fb25c52044080570bd0f7aab3def79ef2a54f42f
njogued <njogued@gmail.com>
2023-09-22 13:03:09 +0300

    script to keep render alive

---
+++ keepalive.py
@@ -0,0 +1,10 @@
+import requests
+import time
+
+while True:
+    response = requests.get("https://servlist.onrender.com/")
+    if response.status_code == 200:
+        print("ping")
+    else:
+        print("Error code:", response.status_code)
+    time.sleep(10)
